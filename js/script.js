7010dbe0c52a53ba0810ad415ff060666878a9dc
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2022-07-29 10:26:07 -0400

    animación menú mobile

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ let elementoMenu = document.getElementsByClassName('icono-menu')[0];
 
 elementoMenu.addEventListener('click', function(event) {
     elementoMenu.classList.toggle('activo');
-
-    // Secciones opacity, se obtienen todas las secciones como un listado HTMLCollection
-    let secciones = document.getElementsByClassName('flex');
-
-    // Se recorre uno por uno los elementos y se le agrega la clase opacity
-    for (var i = 0; i < secciones.length; i++) {
-        secciones[i].classList.toggle('clase-opacity');
-    }
 });
+
+// Submenu desplegable, se obtienen todos los enlaces como un listado HTMLCollection
+let menuPadre = document.getElementsByClassName('enlace');
+
+
+// Se recorre uno por uno los elementos y se le agrega un listener para el click
+for (var i = 0; i < menuPadre.length; i++) {
+    menuPadre[i].addEventListener('click', function(event) {
+        this.classList.toggle('activo');
+    });
+}
